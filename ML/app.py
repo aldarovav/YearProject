c4f70fc436ec9c1dc037c61f9ada8eeead0d0523
aldarovav <aldarov.a.v@gmail.com>
2025-12-05 19:51:25 +0300

    Numpy

--- ML/app.py
+++ ML/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 # Фиксированные признаки
 FEATURES = ['year', 'km_driven', 'mileage', 'engine', 'max_power']
